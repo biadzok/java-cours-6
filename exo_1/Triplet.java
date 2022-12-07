3400f5138c8bb1a862901a203f04707477f69225
Marin Bailhe <43707993+biadzok@users.noreply.github.com>
2022-12-07 13:14:18 +0000

    Initial commit

---
+++ exo_1/Triplet.java
@@ -0,0 +1,25 @@
+public class Triplet<T> {
+  private T premier;
+  private T second;
+  private T troisieme;
+
+  public Triplet();
+
+  public Triplet(T premier, T second, T troisieme) {
+    this.premier = premier;
+    this.second = second;
+    this.troisieme = troisieme;
+  }
+
+  public T getPremier() {
+    return this.premier;
+  }
+
+  public T getSecond() {
+    return this.second;
+  }
+
+  public T getTroisieme() {
+    return this.troisieme;
+  }
+}
